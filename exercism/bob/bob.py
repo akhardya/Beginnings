10e98b16a7b08d03f86f272cb9cf22828b4dfdc0
akhardya <akhardya@gmail.com>
2015-05-13 15:29:02 +0200

    understood the previous mistake of not having taken account what was really tested and the links between both files

--- exercism/bob/bob.py
+++ exercism/bob/bob.py
@@ -2,9 +2,7 @@
 # Skeleton file for the Python "Bob" exercise.
 #
 def hey(sentence):
-    if sentence[-1] == "You are, what, like 15"+ "?":
-        return "Sure."
-    elif sentence[-1] == "Does this cryogenic chamber make me look fat" + "?":
+    if sentence[-1] == "?":
         return "Sure."
     elif sentence == "WATCH OUT!":
         return "Whoa, chill out!"
