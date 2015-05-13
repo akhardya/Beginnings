f758f73a6c3e99cd228fb8dcc9bc02c037d4d64c
akhardya <akhardya@gmail.com>
2015-05-13 14:48:30 +0200

    beginning the refactoring step

--- exercism/bob/bob.py
+++ exercism/bob/bob.py
@@ -2,9 +2,9 @@
 # Skeleton file for the Python "Bob" exercise.
 #
 def hey(sentence):
-    if sentence == "You are, what, like 15?":
+    if sentence == "You are, what, like 15"+ "?":
         return "Sure."
-    elif sentence == "Does this cryogenic chamber make me look fat?":
+    elif sentence == "Does this cryogenic chamber make me look fat" + "?":
         return "Sure."
     elif sentence == "WATCH OUT!":
         return "Whoa, chill out!"
