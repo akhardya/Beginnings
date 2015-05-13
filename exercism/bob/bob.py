ab7da1368b99358197e36a9dc48e909e447a04a7
akhardya <akhardya@gmail.com>
2015-05-13 15:19:15 +0200

    confusion between a green test and a return False in the Python console

--- exercism/bob/bob.py
+++ exercism/bob/bob.py
@@ -2,9 +2,9 @@
 # Skeleton file for the Python "Bob" exercise.
 #
 def hey(sentence):
-    if sentence == "You are, what, like 15"+ "?":
+    if sentence[-1] == "You are, what, like 15"+ "?":
         return "Sure."
-    elif sentence == "Does this cryogenic chamber make me look fat" + "?":
+    elif sentence[-1] == "Does this cryogenic chamber make me look fat" + "?":
         return "Sure."
     elif sentence == "WATCH OUT!":
         return "Whoa, chill out!"
